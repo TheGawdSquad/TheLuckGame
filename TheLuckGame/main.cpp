9ae643208b220cae1b6212036db5cd9707f17331
TheGawdSquad <44072151+TheGawdSquad@users.noreply.github.com>
2018-10-12 14:29:01 -0700

    Initial Commit

---
+++ TheLuckGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TheLuckGame
+//
+//  Created by cougar on 10/12/18.
+//  Copyright © 2018 The Gawd Coding Squad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
